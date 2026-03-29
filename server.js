a88b47d6c686c879055cfc842fe8d090012a285a
merkelovski666-jpg <merkelovski666@gmail.com>
2026-03-29 21:44:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
+
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+const path = require('path');
+
+// Port koji Render.com automatski dodjeljuje ili 3000 za lokalni test
+const PORT = process.env.PORT || 3000;
+
+// Serviranje statičnih fajlova (tvoj index.html i manifest.json)
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Logika za Chat
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+  console.log('Novi korisnik se povezao');
+
+  // Primanje objekta koji sadrži 'name' i 'text'
+  socket.on('chat message', (data) => {
+    // Šaljemo svima (uključujući pošiljaoca)
+    io.emit('chat message', {
+      name: data.name,
+      text: data.text
     });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Korisnik se odjavio');
+  });
 });
 
-http.listen(3000, () => {
-    console.log('GREEN APPLE JE SPREMAN NA PORTU 3000');
+// Pokretanje servera
+http.listen(PORT, () => {
+  console.log(`GREEN APPLE JE LIVE NA PORTU ${PORT}`);
 });

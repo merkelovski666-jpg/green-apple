47396c6abd1962d2f645e80bd38b3192dab5f7ed
merkelovski666-jpg <merkelovski666@gmail.com>
2026-03-29 19:50:03 +0200

    Add files via upload
    server.js, index.html, package.json

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('GREEN APPLE JE SPREMAN NA PORTU 3000');
+});

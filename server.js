7bf27f41f35ca6deeac74d7486e05f715d4cb2e2
merkelovski666-jpg <merkelovski666@gmail.com>
2026-03-30 12:58:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const io = require('socket.io')(server);
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
-    socket.on('join room', (data) => {
-        socket.join(data.room);
-        console.log(`${data.name} je u sobi: ${data.room}`);
+    socket.on('join', (room) => {
+        socket.join(room);
     });
 
     socket.on('chat message', (data) => {
@@ -17,4 +16,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('Server aktivan na portu ' + PORT));
+server.listen(PORT, () => console.log('Server radi na ' + PORT));

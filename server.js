dea8da1da7cca0baebf7287bade3e3ac8ee02cd5
merkelovski666-jpg <merkelovski666@gmail.com>
2026-03-30 13:59:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,28 +5,17 @@ const io = require('socket.io')(server);
 
 app.use(express.static(__dirname));
 
-let blockedUsers = new Set();
-
 io.on('connection', (socket) => {
-    socket.on('join', (data) => {
-        if (blockedUsers.has(data.phone)) {
-            socket.emit('blocked');
-            return;
-        }
-        socket.join(data.room);
-    });
+    socket.on('join', (room) => socket.join(room));
 
     socket.on('chat message', (data) => {
-        if (!blockedUsers.has(data.phone)) {
-            io.to(data.room).emit('chat message', data);
-        }
+        io.to(data.room).emit('chat message', data);
     });
 
-    // Funkcija za blokiranje (samo ako ti pošalješ komandu)
-    socket.on('block user', (phone) => {
-        blockedUsers.add(phone);
+    socket.on('typing', (data) => {
+        socket.to(data.room).emit('typing', data);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('Green Apple Server trči...'));
+server.listen(PORT, () => console.log('Green Apple Pro aktivan!'));
